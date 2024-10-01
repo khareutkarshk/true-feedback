9cf62d4a5b906454c7a299b60d24649885886884
utkarsh <khareu550@gmail.com>
2024-10-02 01:04:31 +0530

    add nav in root

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,13 +5,14 @@ import { Mail } from "lucide-react";
 import Image from "next/image";
 import messages from "@/messages.json";
 import Autoplay from 'embla-carousel-autoplay';
-
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <>
+    <main className="flex flex-col min-h-screen">
       {/* Main content */}
-      <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
+      <Navbar />
+      <div className="flex-grow  flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
         <section className="text-center mb-8 md:mb-12">
           <h1 className="text-3xl md:text-5xl font-bold">
             Dive into the World of Anonymous Feedback
@@ -47,12 +48,12 @@ export default function Home() {
             ))}
           </CarouselContent>
         </Carousel>
-      </main>
-
+      </div>
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
         © 2024 WhisperBox. All rights reserved.
       </footer>
-    </>
+
+    </main>
   );
 }
